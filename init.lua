6b3c42251c506f26418a7354698cfd55e7af636d
FaceDeer <derksenmobile@gmail.com>
2020-02-01 22:53:29 -0700

    convert into a death compass

--- init.lua
+++ init.lua
@@ -1,111 +1,61 @@
 -- compass configuration interface - adjustable from other mods or minetest.conf settings
-ccompass = {}
+death_compass = {}
 
--- default target to static_spawnpoint or 0/0/0
-ccompass.default_target = minetest.setting_get_pos("static_spawnpoint") or {x=0, y=0, z=0}
+local S = minetest.get_translator("death_compass")
+ 
+ -- how many seconds does the death compass work for? 0 for indefinite
+local duration = tonumber(minetest.settings:get("death_compass_duration")) or 0
 
--- Re-calibration allowed
-ccompass.recalibrate = minetest.settings:get_bool("ccompass_recalibrate")
-if ccompass.recalibrate == nil then
-	ccompass.recalibrate = true
-end
-
--- Target restriction
-ccompass.restrict_target = minetest.settings:get_bool("ccompass_restrict_target")
-ccompass.restrict_target_nodes = {}
-local nodes_setting = minetest.settings:get("ccompass_restrict_target_nodes")
-if nodes_setting then
-	nodes_setting:gsub("[^,]+", function(z)
-		ccompass.restrict_target_nodes[z] = true
-	end)
-end
--- Teleport targets
-ccompass.teleport_nodes = {}
-local teleport_nodes_setting = minetest.settings:get("ccompass_teleport_nodes")
-if teleport_nodes_setting then
-	teleport_nodes_setting:gsub("[^,]+", function(z)
-		ccompass.teleport_nodes[z] = true
-	end)
-else
-	ccompass.teleport_nodes["default:mese"] = true
-end
-
-if minetest.settings:get_bool("ccompass_aliasses") then
-	minetest.register_alias("compass:0", "ccompass:0")
-	minetest.register_alias("compass:1", "ccompass:1")
-	minetest.register_alias("compass:2", "ccompass:2")
-	minetest.register_alias("compass:3", "ccompass:3")
-	minetest.register_alias("compass:4", "ccompass:4")
-	minetest.register_alias("compass:5", "ccompass:5")
-	minetest.register_alias("compass:6", "ccompass:6")
-	minetest.register_alias("compass:7", "ccompass:7")
-	minetest.register_alias("compass:8", "ccompass:8")
-	minetest.register_alias("compass:9", "ccompass:9")
-	minetest.register_alias("compass:10", "ccompass:10")
-	minetest.register_alias("compass:11", "ccompass:11")
-end
+local range_to_inactivate = 5
 
 -- set a position to the compass stack
-function ccompass.set_target(stack, param)
-	param = param or {}
-	-- param.target_pos_string
-	-- param.target_name
-	-- param.playername
-
+function set_target(stack, pos, name)
 	local meta=stack:get_meta()
-	meta:set_string("target_pos", param.target_pos_string)
-	if param.target_name == "" then
-		meta:set_string("description", "Compass to "..param.target_pos_string)
-	else
-		meta:set_string("description", "Compass to "..param.target_name)
-	end
-
-	if param.playername then
-		local player = minetest.get_player_by_name(param.playername)
-		minetest.chat_send_player(param.playername, "Calibration done to "..param.target_name.." "..param.target_pos_string)
-		minetest.sound_play({ name = "ccompass_calibrate", gain = 1 }, { pos = player:getpos(), max_hear_distance = 3 })
-	end
+	meta:set_string("target_pos", minetest.pos_to_string(pos))
+	meta:set_string("target_corpse", name)
+	meta:set_int("time_of_death", minetest.get_gametime())
 end
-
 
 -- Get compass target
 local function get_destination(player, stack)
 	local posstring = stack:get_meta():get_string("target_pos")
 	if posstring ~= "" then
 		return minetest.string_to_pos(posstring)
-	else
-		return ccompass.default_target
 	end
 end
 
-local function teleport_above(playername, target, counter)
-	local player = minetest.get_player_by_name(playername)
-	if not player then
-		return
+-- looped ticking sound if there's a duration on this
+local player_ticking = {}
+local function start_ticking(player_name)
+	if not player_ticking[player_name] then
+		player_ticking[player_name] = minetest.sound_play("death_compass_tick_tock",
+			{to_player = player_name, gain = 0.125, loop = true})
 	end
-
-	for i = (counter or 1), 160 do
-		local nodename = minetest.get_node(target).name
-		if nodename == "ignore" then
-			minetest.emerge_area(target, target)
-			minetest.after(0.1, teleport_above, playername, target, i)
-			return
-		end
-
-		if nodename ~= 'air' then
-			target.y = target.y + 1
-		else
-			break
-		end
+end
+local function stop_ticking(player_name)
+	local tick_tock_handle = player_ticking[player_name]
+	if tick_tock_handle then
+		minetest.sound_stop(tick_tock_handle)
+		player_ticking[player_name] = nil
 	end
-	player:setpos(target)
-	return
 end
 
--- get right image number for players compas
+-- get right image number for players compass
 local function get_compass_stack(player, stack)
 	local target = get_destination(player, stack)
-	local pos = player:getpos()
+	if not target then
+		return ItemStack("death_compass:inactive")
+	end
+	local pos = player:get_pos()
+	local dist = vector.distance(pos, target)
+	local player_name = player:get_player_name()
+	
+	if dist < range_to_inactivate then
+		stop_ticking(player_name)
+		minetest.sound_play("death_compass_bone_crunch", {to_player=player_name, gain = 1.0})
+		return ItemStack("death_compass:inactive")	
+	end
+	
 	local dir = player:get_look_horizontal()
 	local angle_north = math.deg(math.atan2(target.x - pos.x, target.z - pos.z))
 	if angle_north < 0 then
@@ -117,134 +67,125 @@ local function get_compass_stack(player, stack)
 
 	-- create new stack with metadata copied
 	local metadata = stack:get_meta():to_table()
-
-	local newstack = ItemStack("ccompass:"..compass_image)
+	local meta_fields = metadata.fields
+	local time_of_death = tonumber(meta_fields.time_of_death)
+	if duration > 0 then
+		local remaining = time_of_death + duration - minetest.get_gametime()
+		if remaining < 0 then
+			stop_ticking(player_name)
+			minetest.sound_play("death_compass_bone_crunch", {to_player=player_name, gain = 1.0})
+			return ItemStack("death_compass:inactive")
+		end
+		start_ticking(player_name)
+		meta_fields.description = S("@1m to @2's corpse, @3s remaining",
+			math.floor(dist), meta_fields.target_corpse, remaining)
+	else
+		meta_fields.description = S("@1m to @2's corpse, died @3s ago",
+			math.floor(dist), meta_fields.target_corpse, minetest.get_gametime() - time_of_death)
+	end
+	
+	local newstack = ItemStack("death_compass:dir"..compass_image)
 	if metadata then
 		newstack:get_meta():from_table(metadata)
-	end
-	if ccompass.usage_hook then
-		newstack = ccompass.usage_hook(newstack, player) or newstack
 	end
 	return newstack
 end
 
--- Calibrate compass on pointed_thing
-local function on_use_function(itemstack, player, pointed_thing)
-	-- possible only on nodes
-	if pointed_thing.type ~= "node" then --support nodes only for destination
-		minetest.chat_send_player(player:get_player_name(), "Calibration can be done on nodes only")
-		return
-	end
-
-	local nodepos = minetest.get_pointed_thing_position(pointed_thing)
-	local node = minetest.get_node(nodepos)
-
-	-- Do teleport to target
-	if ccompass.teleport_nodes[node.name] then
-		teleport_above(player:get_player_name(), get_destination(player, itemstack))
-		return
-	end
-
-	-- recalibration allowed?
-	if not ccompass.recalibrate then
-		local destination = itemstack:get_meta():get_string("target_pos")
-		if destination ~= "" then
-			minetest.chat_send_player(player:get_player_name(), "Compass already calibrated")
-			return
-		end
-	end
-
-	-- target nodes restricted?
-	if ccompass.restrict_target then
-		if not ccompass.restrict_target_nodes[node.name] then
-			minetest.chat_send_player(player:get_player_name(), "Calibration on this node not possible")
-			return
-		end
-	end
-
-	-- check if waypoint name set in target node
-	local nodepos_string = minetest.pos_to_string(nodepos)
-	local nodemeta = minetest.get_meta(nodepos)
-	local waypoint_name = nodemeta:get_string("waypoint_name")
-	local waypoint_pos = nodemeta:get_string("waypoint_pos")
-	local skip_namechange = nodemeta:get_string("waypoint_skip_namechange")
-	local itemmeta=itemstack:get_meta()
-
-	if waypoint_pos and waypoint_pos ~= "" then
-		nodepos_string = waypoint_pos
-	end
-
-
-	if skip_namechange ~= "" then
-		ccompass.set_target(itemstack, {
-				target_pos_string = nodepos_string,
-				target_name = waypoint_name,
-				playername = player:get_player_name()
-			})
-	else
-		-- show the formspec to player
-		itemmeta:set_string("tmp_target_pos", nodepos_string) --just save temporary
-		minetest.show_formspec(player:get_player_name(), "ccompass",
-				"size[10,2.5]" ..
-				"field[1,1;8,1;name;Destination name:;"..waypoint_name.."]"..
-				"button_exit[0.7,2;3,1;cancel;Cancel]"..
-				"button_exit[3.7,2;5,1;ok;Calibrate]" )
-	end
-	return itemstack
-end
-
--- Process the calibration using entered data
-minetest.register_on_player_receive_fields(function(player, formname, fields)
-	if formname == "ccompass" and fields.name and (fields.ok or fields.key_enter) then
-		local stack=player:get_wielded_item()
-		local meta=stack:get_meta()
-		ccompass.set_target(stack, {
-				target_pos_string = meta:get_string("tmp_target_pos"),
-				target_name = fields.name,
-				playername = player:get_player_name()
-			})
-		meta:set_string("tmp_target_pos", "")
-		player:set_wielded_item(stack)
-	end
-end)
-
 -- update inventory
 minetest.register_globalstep(function(dtime)
 	for i,player in ipairs(minetest.get_connected_players()) do
+		local player_name = player:get_player_name()
 		if player:get_inventory() then
 			for i,stack in ipairs(player:get_inventory():get_list("main")) do
 				if i > 8 then
 					break
 				end
-				if string.sub(stack:get_name(), 0, 9) == "ccompass:" then
+				if string.sub(stack:get_name(), 0, 17) == "death_compass:dir" then
 					player:get_inventory():set_stack("main", i, get_compass_stack(player, stack))
+					player_name = nil -- don't stop the sound playing
 				end
 			end
+		end
+		if player_name then
+			stop_ticking(player_name)
 		end
 	end
 end)
 
 -- register items
 for i = 0, 15 do
-	local image = "ccompass_16_"..i..".png"
-	local groups = {}
-	if i > 0 then
-		groups.not_in_creative_inventory = 1
-	end
-	minetest.register_tool("ccompass:"..i, {
-		description = "Compass",
+	local image = "death_compass_16_"..i..".png"
+	local groups = {death_compass = 1, not_in_creative_inventory = 1}
+	minetest.register_tool("death_compass:dir"..i, {
+		description = S("Death Compass"),
 		inventory_image = image,
 		wield_image = image,
+		stack_max = 1,
 		groups = groups,
-		on_use = on_use_function,
 	})
 end
+minetest.register_tool("death_compass:inactive", {
+	description = S("Inactive Death Compass"),
+	inventory_image = "death_compass_inactive.png",
+	wield_image = "death_compass_inactive.png",
+	stack_max = 1,
+	groups = {death_compass = 1},
+})
 
 minetest.register_craft({
-	output = 'ccompass:0',
+	output = 'death_compass:inactive',
 	recipe = {
-		{'', 'default:steel_ingot', ''},
-		{'default:steel_ingot', 'default:mese_crystal_fragment', 'default:steel_ingot'},
-		{'', 'default:steel_ingot', ''}
+		{'', 'bones:bones', ''},
+		{'bones:bones', 'default:mese_crystal_fragment', 'bones:bones'},
+		{'', 'bones:bones', ''}
 	}
 })
+
+local player_death_location = {}
+minetest.register_on_dieplayer(function(player, reason)
+	local player_name = player:get_player_name()
+	local inv = minetest.get_inventory({type="player", name=player:get_player_name()})
+	local list = inv:get_list("main")
+	local count = 0
+	for i, itemstack in pairs(list) do
+		if minetest.get_item_group(itemstack:get_name(), "death_compass") > 0 then
+			count = count + itemstack:get_count()
+			list[i] = ItemStack("")
+		end
+	end
+	if count > 0 then
+		inv:set_list("main", list)
+		player_death_location[player_name] = {count=count,pos=player:get_pos()}
+	end
+	
+end)
+-- Called when a player dies
+-- `reason`: a PlayerHPChangeReason table, see register_on_player_hpchange
+
+minetest.register_on_respawnplayer(function(player)
+	local player_name = player:get_player_name()
+	local compasses = player_death_location[player_name]
+	if compasses then
+		local inv = minetest.get_inventory({type="player", name=player_name})
+		
+		-- Remove any death compasses they might still have for some reason
+		local current = inv:get_list("main")
+		for i, item in pairs(current) do
+			if minetest.get_item_group(item:get_name(), "death_compass") > 0 then
+				current[i] = ItemStack("")
+			end
+		end
+		inv:set_list("main", current)
+		
+		-- give them new compasses pointing to their place of death
+		for i = 1, compasses.count do
+			local compass = ItemStack("death_compass:dir0")
+			set_target(compass, compasses.pos, player_name)
+			inv:add_item("main", compass)
+		end
+	end
+	return false
+end)
+--    * Called when player is to be respawned
+--    * Called _before_ repositioning of player occurs
+--    * return true in func to disable regular player placement
